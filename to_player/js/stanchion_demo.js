38021e013059bcb07df72e3b26990c22c89720b4
John Moons <j.moons@realdigitalmedia.com>
2015-11-13 18:06:32 -0500

    Cleaning up some nasty duplication, keep it dry sucka!

--- to_player/js/stanchion_demo.js
+++ to_player/js/stanchion_demo.js
@@ -1,65 +1,33 @@
 $(document).ready(function() {
 
-  var start_json_text = "Start JSON AJAX Interval";
-  var start_xml_text  = "Start XML AJAX Interval";
-
-  var stop_json_text  = "Stop JSON AJAX Interval";
-  var stop_xml_text   = "Stop XML AJAX Interval";
+  var start_text = "Start AJAX Interval";
+  var stop_text  = "Stop AJAX Interval";
 
   // Initialize Start/Stop AJAX Button
-  $("#json_button").addClass('stopped');
-  $("#xml_button").addClass('stopped');
+  $(".ajax_button").addClass('stopped');
 
-  $("#json_button").text(start_json_text);
-  $("#xml_button").text(start_xml_text);
+  var ajax_interval_id  = null;
 
-  var json_interval_id  = null;
-  var xml_interval_id   = null;
-
-  $("#json_button").on('click', function() {
+  $(".ajax_button").on('click', function() {
     var $button = $(this);
 
+    var ajax_data_type = $button.data('request_format');
     $button.toggleClass("stopped");
 
     if ( $button.hasClass("stopped") ) {
       // I am running, stop interval
-      $button.text(start_json_text);
-      clearInterval(json_interval_id);
+      $button.text(start_text + " " + ajax_data_type.toUpperCase() );
+      clearInterval(ajax_interval_id);
     } else {
       // I am not running, start interval
-      $button.text(stop_json_text);
-      json_interval_id = setInterval( function() {
+      $button.text(stop_text + " " + ajax_data_type.toUpperCase() );
+
+      var url = "/outgoing_stanchion_data_" + ajax_data_type
+      ajax_interval_id = setInterval( function() {
                           $.ajax({
                               method:   "GET",
-                              url:      "/outgoing_stanchion_data_json",
-                              dataType: "json"
-                            }
-                          ).done( function(data) {
-                            console.log(data);
-                          }).fail( function(data) {
-                            console.log("UH OH GET FAILED")
-                          });
-                        }, 3000);
-    };
-  });
-
-  $("#xml_button").on('click', function() {
-    var $button = $(this);
-
-    $button.toggleClass("stopped");
-
-    if ( $button.hasClass("stopped") ) {
-      // I am running, stop interval
-      $button.text(start_xml_text);
-      clearInterval(xml_interval_id);
-    } else {
-      // I am not running, start interval
-      $button.text(stop_xml_text);
-      xml_interval_id = setInterval( function() {
-                          $.ajax({
-                              method:   "GET",
-                              url:      "/outgoing_stanchion_data_xml",
-                              dataType: "xml"
+                              url:      url,
+                              dataType: ajax_data_type
                             }
                           ).done( function(data) {
                             console.log(data);
