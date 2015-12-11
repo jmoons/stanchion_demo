0a745a4d644707d63765f096c019337fb25f6adb
John Moons <j.moons@realdigitalmedia.com>
2015-12-11 15:09:18 -0500

    swapping and staging logic in place

--- to_player/js/stanchion_demo.js
+++ to_player/js/stanchion_demo.js
@@ -6,36 +6,60 @@ $(document).ready(function() {
 
 var StanchionDemo = ( function() {
 
+  var stage_triggered_content = function() {
+    var timeout = setTimeout( function() {
+      window.clearTimeout(timeout);
+      reset_to_default();
+    }, StanchionDemo.TRIGGERED_CONTENT_DISPLAY_SECONDS * 1000);
+  }
+
+  var reset_to_default = function() {
+    console.log("RESET TO DEFAULT");
+    $("#image_container").css( 'background-image', 'url(images/necam-fs-default.png)' );
+    begin_polling_for_data();
+  }
+
   var check_and_act_upon_triggered_content = function( stanchion_xml ) {
     var golf_clubs_plu = "2000000368177";
     var polo_shirt_plu = "200004528782";
 
     var plus_in_stanchion_xml = $(stanchion_xml).find('numPLU').toArray();
+    var found_matching_plu = false;
     plus_in_stanchion_xml.forEach( function( plu, index ) {
       if ( $(plu).text() === golf_clubs_plu ) {
+        found_matching_plu = true;
         console.log("GOLF CLUBS!!!");
+        $("#image_container").css( 'background-image', 'url(images/necam-fs-golf.png)' );
+        stage_triggered_content();
       } else if ( $(plu).text() === polo_shirt_plu ) {
+        found_matching_plu = true;
         console.log("POLO SHIRT!!!");
-      };
+        $("#image_container").css( 'background-image', 'url(images/necam-fs-apparel.png)' );
+        stage_triggered_content();
+      }
     });
+
+    if (!found_matching_plu) {
+      begin_polling_for_data();
+    };
+
   }
 
   var begin_polling_for_data = function() {
 
     (function long_poll() {
-      setTimeout(function(){
+      var timer = setTimeout(function(){
         $.ajax({
             url: StanchionDemo.IP_ADDRESS_TO_USE + "/outgoing_stanchion_data_xml",
             method: "GET",
             dataType: "XML"
           }).done(function(data) {
             console.log("GET SUCCESS");
+            window.clearTimeout( timer );
             check_and_act_upon_triggered_content( data )
           }).fail(function(data) {
             console.log("GET FAILED");
-          }).always(function(data) {
-            console.log("GET ALWAYS - REPOLLING");
-            //Setup the next poll recursively
+            window.clearTimeout( timer );
             long_poll();
           });
       }, (StanchionDemo.POLLING_RATE_SECONDS * 1000) );
@@ -46,21 +70,12 @@ var StanchionDemo = ( function() {
   return {
     initialize_demo: function() {
 
-      this.IP_ADDRESS_TO_USE    = "http://192.168.25.100:9292";
-      this.POLLING_RATE_SECONDS = 5;
+      this.IP_ADDRESS_TO_USE                  = "http://192.168.25.100:9292";
+      this.POLLING_RATE_SECONDS               = 3;
+      this.TRIGGERED_CONTENT_DISPLAY_SECONDS  = 10;
 
       begin_polling_for_data();
 
     }
   }
 })();
-
-// HOW TO PARSE XML INPUT
-// $(data).find('numPLU')
-// [<numPLU>​2400056785650​</numPLU>​, <numPLU>​2400056785643​</numPLU>​]
-
-// test = test[0]
-// <numPLU>​2400056785650​</numPLU>​
-
-// test.textContent
-// "2400056785650"
