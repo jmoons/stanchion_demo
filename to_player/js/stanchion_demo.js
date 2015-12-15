47133d14b126a5f7383f6e9ab62a34b8a4ee3d15
John Moons <j.moons@realdigitalmedia.com>
2015-12-15 11:42:42 -0500

    1) Allowing multiple matching items triggered content to be honored - display the triggered content in the order it appears in the transaction
    2) Changing the web server behavior - now base decision to check for triggered content off the MD5 sum of the posted XML

--- to_player/js/stanchion_demo.js
+++ to_player/js/stanchion_demo.js
@@ -6,11 +6,23 @@ $(document).ready(function() {
 
 var StanchionDemo = ( function() {
 
-  var stage_triggered_content = function() {
-    var timeout = setTimeout( function() {
-      window.clearTimeout(timeout);
-      reset_to_default();
-    }, StanchionDemo.TRIGGERED_CONTENT_DISPLAY_SECONDS * 1000);
+  var stage_triggered_content = function( triggered_content_to_stage ) {
+    // var timeout = setTimeout( function() {
+    //   window.clearTimeout(timeout);
+    //   reset_to_default();
+    // }, StanchionDemo.TRIGGERED_CONTENT_DISPLAY_SECONDS * 1000);
+
+    function display_triggered_content() {
+      $("#image_container").css( 'background-image', triggered_content_to_stage.shift() );
+
+      if ( triggered_content_to_stage.length > 0 ) {
+        setTimeout( display_triggered_content, StanchionDemo.TRIGGERED_CONTENT_DISPLAY_SECONDS * 1000 );
+      } else {
+        setTimeout( reset_to_default, StanchionDemo.TRIGGERED_CONTENT_DISPLAY_SECONDS * 1000 )
+      };
+    }
+
+    display_triggered_content();
   }
 
   var reset_to_default = function() {
@@ -22,24 +34,23 @@ var StanchionDemo = ( function() {
   var check_and_act_upon_triggered_content = function( stanchion_xml ) {
 
     var plus_in_stanchion_xml = $(stanchion_xml).find('numPLU').toArray();
-    var found_matching_plu    = false;
+
+    var triggered_content_to_stage = [];
 
     plus_in_stanchion_xml.forEach( function( plu, index ) {
       if ( $(plu).text() === StanchionDemo.GOLF_CLUBS_PLU ) {
-        found_matching_plu = true;
         console.log("GOLF CLUBS!!!");
-        $("#image_container").css( 'background-image', 'url(images/necam-fs-golf.png)' );
-        stage_triggered_content();
+        triggered_content_to_stage.push('url(images/necam-fs-golf.png)')
       } else if ( $(plu).text() === StanchionDemo.POLO_SHIRT_PLU ) {
-        found_matching_plu = true;
         console.log("POLO SHIRT!!!");
-        $("#image_container").css( 'background-image', 'url(images/necam-fs-apparel.png)' );
-        stage_triggered_content();
+        triggered_content_to_stage.push('url(images/necam-fs-apparel.png)')
       }
     });
 
-    if (!found_matching_plu) {
+    if ( triggered_content_to_stage.length == 0 ) {
       begin_polling_for_data();
+    } else {
+      stage_triggered_content( triggered_content_to_stage );
     };
 
   }
@@ -52,10 +63,20 @@ var StanchionDemo = ( function() {
             url: StanchionDemo.IP_ADDRESS_TO_USE + "/outgoing_stanchion_data_xml",
             method: "GET",
             dataType: "XML"
-          }).done(function(data) {
+          }).done(function( data, response_text, request_headers ) {
             console.log("GET SUCCESS");
             window.clearTimeout( timer );
-            check_and_act_upon_triggered_content( data )
+            if ( request_headers.getResponseHeader("MD5_SUM") === StanchionDemo.last_seen_md5 ) {
+              // Do Nothing
+              console.log("MATCHING MD5 - Continue Polling");
+              long_poll();
+            } else {
+              console.log("DIFFERING MD5 - CHECKING");
+              // Update the last-seen MD5
+              StanchionDemo.last_seen_md5 = request_headers.getResponseHeader("MD5_SUM");
+              check_and_act_upon_triggered_content( data )
+            };
+            // check_and_act_upon_triggered_content( data )
           }).fail(function(data) {
             console.log("GET FAILED");
             window.clearTimeout( timer );
@@ -70,7 +91,7 @@ var StanchionDemo = ( function() {
     initialize_demo: function() {
 
       // this.IP_ADDRESS_TO_USE                  = "https://shrouded-temple-8115.herokuapp.com";
-      this.IP_ADDRESS_TO_USE                  = "http://192.168.25.100:9292";
+      this.IP_ADDRESS_TO_USE                  = "http://192.168.25.13:9292";
       this.POLLING_RATE_SECONDS               = 5;
       this.TRIGGERED_CONTENT_DISPLAY_SECONDS  = 5;
       this.GOLF_CLUBS_PLU                     = "2000031744865";
